6fd01ab776cbe29e65fa4544847a274162d4f7ce
kleiber <kleiber@adobe.com>
2021-06-08 10:54:14 +0200

    A1

--- JSFApp/JavaSource/ch/bbzbl/entity/Person.java
+++ JSFApp/JavaSource/ch/bbzbl/entity/Person.java
@@ -21,6 +21,15 @@ public class Person implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private int id;
 	private String name;
+	private int age;
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
 
 	@ManyToMany
 	private List<Language> languages;
